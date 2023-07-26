3d68c48d0b5c015372729db00ea4edfd675b65b5
yeyo27 <yeyo1mm@gmail.com>
2023-07-26 21:47:38 +0200

    Added Point and Line classes to implement Window.draw_line()

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x, self.y = x, y
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1, self.p2 = p1, p2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.p1.x, self.p1.y,
+            self.p2.x, self.p2.y,
+            fill=fill_color, width=2
+        )
+        canvas.pack()
+
+
 class Window:
     def __init__(self, width: int, height: int):
         self.__root = Tk()
@@ -22,9 +40,16 @@ class Window:
     def close(self):
         self.window_is_running = False
 
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.canvas, fill_color)
+
 
 def main():
     win = Window(800, 600)
+    p1 = Point(20, 20)
+    p2 = Point(400, 400)
+    line = Line(p1, p2)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 
